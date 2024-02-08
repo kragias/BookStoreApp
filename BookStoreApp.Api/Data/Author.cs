444aab7cd01085e4f5a27b4e5d028e28deb6bc18
kostas <kostas.ragias@gmail.com>
2024-02-08 17:39:42 +0200

    book entity

--- BookStoreApp.Api/Data/Author.cs
+++ BookStoreApp.Api/Data/Author.cs
@@ -11,8 +11,8 @@ namespace BookStoreApp.Api.Data
         }
 
         public int Id { get; set; }
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+        public string? FirstName { get; set; } 
+        public string? LastName { get; set; } 
         public string? Bio { get; set; }
 
         public virtual ICollection<Book> Books { get; set; }
--- BookStoreApp.Api/Data/Book.cs
+++ BookStoreApp.Api/Data/Book.cs
@@ -6,12 +6,12 @@ namespace BookStoreApp.Api.Data
     public partial class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; } = null!;
+        public string? Title { get; set; }
         public int? Year { get; set; }
         public string Isbn { get; set; } = null!;
-        public string Summary { get; set; } = null!;
-        public string Image { get; set; } = null!;
-        public double? Price { get; set; }
+        public string? Summary { get; set; } 
+        public string? Image { get; set; } 
+        public decimal? Price { get; set; }
         public int? AuthorId { get; set; }
 
         public virtual Author? Author { get; set; }
