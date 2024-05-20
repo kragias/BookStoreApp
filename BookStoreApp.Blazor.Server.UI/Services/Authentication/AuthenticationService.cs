09b85066cf1c698277113e7f2276b9415313019b
kostas <kostas.ragias@gmail.com>
2024-05-20 19:51:07 +0300

    Completed setup of Blazor Server App and Authentication

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    using Blazored.LocalStorage;
+    using BookStoreApp.Blazor.Server.UI.Providers;
+    using BookStoreApp.Blazor.Server.UI.Services.Base;
+    using Microsoft.AspNetCore.Components.Authorization;
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+        private readonly ILocalStorageService localStorage;
+        private readonly AuthenticationStateProvider authenticationStateProvider; //i inject the original AuthenticationStateProvider
+
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.httpClient = httpClient;
+            this.localStorage = localStorage;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> AuthenticateAsync(LoginUserDto loginModel)
+        {
+            var response = await httpClient.LoginAsync(loginModel);
+
+            //Store Token
+            await localStorage.SetItemAsync("accessToken", response.Token);
+
+            //Change auth state of app. I must cast to my custom ApiAuthenticationStateProvider type
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedOut();
+        }
+    }
+}
---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    using BookStoreApp.Blazor.Server.UI.Services.Base;
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+        public Task Logout();
+    }
+}
